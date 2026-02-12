fbec6126d6afb6728d433fdf1a8c4cbe3b14dc56
Ansa <ansakanwal2145@gmail.com>
2026-02-12 13:07:40 -0500

    AK-created Mocks for test

--- src/test/java/ProgramControlTest.java
+++ src/test/java/ProgramControlTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ProgramControlTest {
+
+    //create Mock of Filehandler
+
+    @Mock
+    FileHandler fileHandler;
+
+    //create Mock of CipherService
+    @Mock
+    CipherService cipherService;
+
+    //use actual programControl.java
+
+    @InjectMocks
+    ProgramControl programControl;
+    //every test will be called like programControl.method name
 }
